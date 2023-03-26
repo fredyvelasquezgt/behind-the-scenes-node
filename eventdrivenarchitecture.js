6e92aca1fac3248451bee722663e5abce76a42bc
Fredy Velasquez <macacoloco696@gmail.com>
2023-03-25 20:58:54 -0600

    create events

--- eventdrivenarchitecture.js
+++ eventdrivenarchitecture.js
@@ -1,1 +1,13 @@
 let events = require('events');
+
+//creo un nuevo eventEmitter
+let eventEmitter = new events.EventEmitter();
+
+//listen the event - this is the listener
+eventEmitter.on('connection',() => {
+    console.log('Connection succesfull.');
+})
+
+//emitir el evento or fire the event
+eventEmitter.emit('connection');
+eventEmitter.emit('connections');
